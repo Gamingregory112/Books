60905af3330f46138adf2a9de903f01b001bf108
Gamingregory112 <104397063+Gamingregory112@users.noreply.github.com>
2023-09-15 09:52:16 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+var objPeople = [
+  {
+    username: "same",
+    password: "same"
+  }
+]
 
+function getInfo() {
+  var username = document.getElementById("username").value
+  var password = document.getElementById("password").value
+
+  for (i = 0; i < objPeople.length; i++) {
+    if (username == objPeople[i].username && password == objPeople[i].password) {
+      
+    }
+  }
+}
