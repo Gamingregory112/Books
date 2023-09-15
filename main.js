6ed696f625058b012715fdc479e1c0e4a4b8f533
Gamingregory112 <104397063+Gamingregory112@users.noreply.github.com>
2023-09-15 10:28:38 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ var objPeople = [
 function getInfo() {
   var username = document.getElementById("username").value
   var password = document.getElementById("password").value
+  window.alert(username + " " + password);
 
   for (i = 0; i < objPeople.length; i++) {
     if (username == objPeople[i].username && password == objPeople[i].password) {
